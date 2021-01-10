463ce9788a40ef3ccb0b19d652f60b7efb76db91
bobmeme <bobmeme@users.noreply.github.com>
2021-01-10 02:51:09 +0100

    changed name

--- web/Controllers/Api/GroupApiController.cs
+++ web/Controllers/Api/GroupApiController.cs
@@ -10,7 +10,7 @@ using web.Models;
 
 namespace web.Controllers_Api
 {
-    [Route("api/v1/group")]
+    [Route("api/v1/Group")]
     [ApiController]
     public class GroupApiController : ControllerBase
     {
