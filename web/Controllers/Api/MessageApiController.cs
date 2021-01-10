6ca344e592a6fc2ca797aff85b530a83f0d4c936
bobmeme <bobmeme@users.noreply.github.com>
2021-01-10 18:15:49 +0100

    added event and message api

---
+++ web/Controllers/Api/MessageApiController.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using web.Data;
+using web.Models;
+
+namespace web.Controllers_Api
+{
+    [Route("api/v1/Message")]
+    [ApiController]
+    public class MessageApiController : ControllerBase
+    {
+        private readonly CloudContext _context;
+
+        public MessageApiController(CloudContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MessageApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Message>>> GetMessages()
+        {
+            return await _context.Messages.ToListAsync();
+        }
+
+        // GET: api/MessageApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Message>> GetMessage(int id)
+        {
+            var message = await _context.Messages.FindAsync(id);
+
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return message;
+        }
+
+        // PUT: api/MessageApi/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMessage(int id, Message message)
+        {
+            if (id != message.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(message).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MessageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MessageApi
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Message>> PostMessage(Message message)
+        {
+            _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMessage", new { id = message.ID }, message);
+        }
+
+        // DELETE: api/MessageApi/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Message>> DeleteMessage(int id)
+        {
+            var message = await _context.Messages.FindAsync(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            _context.Messages.Remove(message);
+            await _context.SaveChangesAsync();
+
+            return message;
+        }
+
+        private bool MessageExists(int id)
+        {
+            return _context.Messages.Any(e => e.ID == id);
+        }
+    }
+}
